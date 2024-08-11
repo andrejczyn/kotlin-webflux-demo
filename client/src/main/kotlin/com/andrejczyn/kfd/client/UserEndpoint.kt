97e4458ab890a02827515f14bc67a4c7e82a5d26
horhe <horhe@merxu.com>
2024-08-11 11:34:44 +0200

    Users endpoint

--- client/src/main/kotlin/com/andrejczyn/kfd/client/UserEndpoint.kt
+++ client/src/main/kotlin/com/andrejczyn/kfd/client/UserEndpoint.kt
@@ -1,4 +1,31 @@
 package com.andrejczyn.kfd.client
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+import java.util.UUID
+
+@RestController
+@RequestMapping(path = ["/users"])
 class UserEndpoint {
+    @GetMapping("/{userId}")
+    suspend fun user(
+        @PathVariable userId: UUID,
+    ): User {
+        return User(
+            id = userId,
+            firstname = "John",
+            surname = "Kowalski",
+            createdAt = Instant.now(),
+        )
+    }
 }
+
+data class User(
+    val id: UUID,
+    val firstname: String,
+    val surname: String,
+    val createdAt: Instant,
+)
